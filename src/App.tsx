57581a53508e2c6640ecdf12516f32e35c1b1cc7
david keukeu <davidkeukeu8@gmail.com>
2023-06-09 22:30:31 +0200

    finish the pokemonDetail component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import { FunctionComponent } from "react";
 import PokemonList from "./pages/pokemons-list";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import PokemonsDetail from "./pages/pokemon-details";
+import PageNotFound from "./pages/page-not-found";
 
 const App: FunctionComponent = () => {
-  return <PokemonList />;
+  return (
+    <Router>
+      <div>
+        {/* la barre de navigation commune a toute l'application */}
+        <nav className="nav-wrapper teal">
+          <Link to="/" className="brand-logo center">
+            Pokedex
+          </Link>
+        </nav>
+        {/** systeme de gestion des routes de notre application  */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonsDetail} />
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -3,6 +3,7 @@ import Pokemon from "../models/pokemon";
 import "./pokemon-card.css";
 import formatDate from "../helpers/format-date";
 import formatType from "../helpers/format-type";
+import { useHistory } from "react-router-dom";
 
 type Props = {
   pokemon: Pokemon;
@@ -14,6 +15,7 @@ const PokemonCard: FunctionComponent<Props> = ({
   borderColor = "#009688",
 }) => {
   const [color, setColor] = useState<string>();
+  const history = useHistory();
 
   const showBorder = () => {
     setColor(borderColor);
@@ -23,11 +25,16 @@ const PokemonCard: FunctionComponent<Props> = ({
     setColor("#f5f5f5");
   };
 
+  const goToPokemon = (id: number) => {
+    history.push(`/pokemons/${id}`);
+  };
+
   return (
     <div
       className="col s6 m4"
       onMouseEnter={showBorder}
       onMouseLeave={hideBorder}
+      onClick={() => goToPokemon(pokemon.id)}
     >
       <div className="card horizontal" style={{ borderColor: color }}>
         <div className="card-image">
@@ -38,9 +45,6 @@ const PokemonCard: FunctionComponent<Props> = ({
             <p>{pokemon.name}</p>
             <p>
               <small>{formatDate(pokemon.created)}</small>
-            </p>
-            <p>
-              <small>{pokemon.created.toString()}</small>
             </p>
             {pokemon.types.map((type) => (
               <span key={type} className={formatType(type)}>
