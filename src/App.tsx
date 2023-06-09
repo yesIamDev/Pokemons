8db21e9e38611a97b5c4627459abd2c6f3927fbc
david keukeu <davidkeukeu8@gmail.com>
2023-06-09 16:34:18 +0200

    fix all bugs in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemons-list";
+import { Router, Switch } from "react-router";
+import { Link } from "react-router-dom";
 
-function App() {
+const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        {/* la barre de navigation commune a toutes les pages */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">Pokedex</Link>
+          </div>
+        </nav>
+        {/* systeme de gestion des routes dans notre application */}
+        
+      </div>
+    </Router>
+  )
+};
 
 export default App;
